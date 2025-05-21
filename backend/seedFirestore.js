8fb199ef34ae8cea570c58067a5a8784260ac024
attabooi <joonqpa@gmail.com>
2025-05-21 10:25:51 +0900

    seed

--- backend/seedFirestore.js
+++ backend/seedFirestore.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./your-service-account-file.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
